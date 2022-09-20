4842b0a511870a7212f70001cb560306c50ea909
Кадников Николай <25roter52@gmail.com>
2022-09-20 22:15:39 +0400

    add: find and save pictures in folders

--- lab1.py
+++ lab1.py
@@ -27,10 +27,28 @@
 
 
 
-!pip install beautifulsoup4
+import os
+path = '/home/cossieman2000/WORK/python/'
+project_name = 'dataset'
+folders = ['polar bear', 'brown bear']
 
+def createFolder(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+fullpath = os.path.join(path, project_name)
+print(fullpath)
+createFolder(fullpath)
+
+for f in folders:
+    folder = os.path.join(fullpath, f)
+    print(folder)
+    createFolder(folder)
+
+
+!pip install BeautifulSoup4
 import requests
-from bs4 import BeautifulStoneSoup
+from bs4 import BeautifulSoup
 
 url = "https://yandex.ru/images/search?text=polar%20bear"
 r = requests.get(url)
@@ -39,22 +57,28 @@ r.content
 soup = BeautifulSoup(r.content, 'html')
 soup
 
-blocks = soup.findAll('a', class_='serp-item__link')#нашли одну картинку
+import time ## скачивание белых медведей, ошибка безопасного подключения
+import validators
+blocks = soup.findAll('div', class_='serp-item__preview')#нашли одну картинку
+number = 0
 for block in blocks:
-    print('1')
     if (block):
-        download_block = block.find('img', class_='serp-item__thumb')
-        if  download_block:
-            fine_link = download_block.get('src')
-            if  fine_link:
-                print(fine_link)
-                image_final_link = 'https:' + fine_link
-                print(image_final_link)
-            else:
-                    print('no fine_link')
+        download_link = block.find('a', class_='serp-item__link').get('href')
+        
+        final_link = (download_link.split('=')[2])
+        final_link1 = final_link.replace('%3A', ':')
+        final_link2 = final_link1.replace('%2F', '/')
+        final_link3 = (final_link2.split('&')[0])
+        print(final_link3)
+        if validators.url(final_link3):
+            print('1')
+            image_bytes = requests.get(f'{final_link3}').content
+            time.sleep(1)
+            with open(f'/home/cossieman2000/WORK/python/dataset/polar bear/{number}.jpg', 'wb') as file:
+                file.write(image_bytes)
+            number += 1
+            print(number)
         else:
-                print('no data-thumb')
-    else:
-        print('none')
-
-print('end')
+            continue
+    #else:
+    #    print('none')
