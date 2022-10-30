fe9858066e4810db389bccd6e9cb5aeae35baad8
Кадников Николай <25roter52@gmail.com>
2022-10-30 20:20:11 +0400

    add: downloaded all the photos, made new annotations and datasetes

--- lab1.py
+++ lab1.py
@@ -1,41 +1,21 @@
+
 import os
-path = '/home/cossieman2000/WORK/python/'
-project_name = 'dataset'
-folders = ['polar bear', 'brown bear']
-
-def createFolder(path):
-    if not os.path.exists(path):
-        os.mkdir(path)
-
-fullpath = os.path.join(path, project_name)
-print(fullpath)
-createFolder(fullpath)
-
-for f in folders:
-    folder = os.path.join(fullpath, f)
-    print(folder)
-    createFolder(folder)
-
-!pip install requests
-!pip install bs4
-!pip install opencv-python
-
 import requests
 from bs4 import BeautifulSoup
 import time
-
-url1_1 = "https://yandex.ru/images/search?p=" 
-url1_2 = "&text=polar%20bear"
-url2_2 = "&text=brown%20bear"
-
-import random
-import cv2
-
 import random
 import cv2
 from skimage import io
 
-def PictDownload (url1, url2, number, color, fullpath):
+'''!pip install requests
+!pip install bs4
+!pip install opencv-python
+!pip install scikit-image'''
+
+
+
+
+def pict_download (url1, url2, number, color, fullpath):
     for page in range(0, 37):
         url = url1 + str(page) + url2
         r = requests.get(url)
@@ -60,77 +40,67 @@ def PictDownload (url1, url2, number, color, fullpath):
                     with open(image_path, 'wb') as file:
                         file.write(image_bytes)
                     try:
-                        io.imread('/home/cossieman2000/WORK/python/dataset/polar bear/0000.jpg')
+                        io.imread(image_path)
                         number += 1
                         print(number)
-                        print('normal image')
                         image = cv2.imread(image_path)
                         cv2.imwrite(image_path, image)
-                        print(image.shape)
                     except:
                         print('broken image')
                         continue
     print(page)
+    """
+    функция: скачиваем фото в соответствующие папки
+    """
+def create_folder(path):
+        if not os.path.exists(path):
+            os.mkdir(path)
 
-number = 0
-PictDownload(url1_1, url1_2, number)
 
-number = 0
-PictDownload(url1_1, url2_2, number)
 
-class Iterator_1:
-    def __init__(self, class_name):
-        print(class_name)
-        self.class_name = class_name
-        self.counter = 0
 
-    def __next__(self):
-        photo_path = '/home/cossieman2000/WORK/python/dataset/' + self.class_name + '/' + str(self.counter).zfill(4) + '.jpg'
-        if os.path.exists(photo_path):
-            self.counter += 1
-            print(photo_path)
-            return photo_path
-        else:
-            raise StopIteration
 
-path = '/home/cossieman2000/WORK/python/'
-annotation_name = 'annotation_1.csv'
 
-filepath = os.path.join(path, annotation_name)
-annotation_1 = open(filepath, "w")
-annotation_1.close()
 
-import pandas as pd
 
-iter1 = Iterator_1('polar bear')
-'''while(1):
-    photo_path = next(iter1)
-    print(photo_path)'''
-        #abs_path = #обработать photo_path таk, ктоб тут был абсолютный путь до фото
-        #rel_path = #относительный путь(только датасет и папки)
-        #photo_class = #буквенное название класса(полярный или бурый медведь), и записываем в таблицу
-path = '/home/cossieman2000/WORK/python/'
-annotation_name = 'annotation_1.csv'
 
-filepath = os.path.join(path, annotation_name)
 
-def WriteAnnotation(iter1):
-    #while(1):
-    columns = ['absolute path', 'relative path', 'class name']
-    annotation_1 = open(filepath, "a+")
-    for i in range(0,5):
-        #try:
-        photo_path = next(iter1)
-        print(photo_path)
-        relative_path = photo_path.split('WORK')[1]
-        print(relative_path)
-        class_name = relative_path.split('dataset/')[1].split(' bear')[0] + ' bear'
-        print(class_name)
-        datas = [photo_path, relative_path, class_name]
-        data = pd.DataFrame(datas, columns=columns)
-        data.to_csv(annotation_1, mode = 'a', index = False)
-        #except:
-            #break
-    annotation_1.close()
+if __name__=="__main__":
 
-WriteAnnotation(iter1)
+
+    url1_1 = "https://yandex.ru/images/search?p=" 
+    url1_2 = "&text=polar%20bear"
+    url2_2 = "&text=brown%20bear"
+    path = '/home/cossieman2000/WORK/python/'
+    project_name = 'dataset'
+    folders = ['polar bear', 'brown bear']
+
+
+
+    fullpath = os.path.join(path, project_name)
+    print(fullpath)
+    create_folder(fullpath)
+
+
+    for f in folders:
+        folder = os.path.join(fullpath, f)
+        print(folder)
+        create_folder(folder)
+        """ 
+        создаем папки dataset, polar bear, brown bear
+        """
+
+
+
+
+    number = 0
+    #pict_download(url1_1, url1_2, number, 'polar', fullpath)
+    """
+    скачиваем белых медведей
+    """
+
+    number = 0
+    #pict_download(url1_1, url2_2, number, 'brown', fullpath)
+    """
+    скачиваем бурых медведей
+    """
---
+++ lab2.py
@@ -0,0 +1,202 @@
+
+import lab1
+import os
+import csv
+import shutil
+import glob
+import random
+
+class iterator_1:
+    def __init__(self, class_name):
+        print(class_name)
+        self.class_name = class_name
+        self.counter = 0
+
+    def __next__(self):
+        photo_path = '/home/cossieman2000/WORK/python/dataset/' + self.class_name + '/' + str(self.counter).zfill(4) + '.jpg'
+        if os.path.exists(photo_path):
+            self.counter += 1
+            print(photo_path)
+            return photo_path
+        else:
+            raise StopIteration
+"""
+класс итератор
+"""
+
+
+
+
+def write_annotation(iter1, annotation_name):
+    while(True):
+        try:
+            photo_path = next(iter1)
+            print(photo_path)
+            relative_path = photo_path.split('WORK')[1]
+            print(relative_path)
+            class_name = relative_path.split('dataset/')[1].split(' bear')[0] + ' bear'
+            print(class_name)
+            with open(annotation_name, mode="a", encoding='utf-8') as write_file:
+                file_writer = csv.writer(write_file, delimiter = ",", lineterminator="\r")
+                file_writer.writerow((photo_path, relative_path, class_name))
+        except:
+            break
+
+
+    """
+    Функция: Запись фалов в 1ю аннотацию
+    """
+
+
+
+
+def copying_dataset_1(Iter, annotation_name, new_path):
+    while(1):
+        try: 
+            print(new_path)         
+            photo_path = next(Iter)
+            print(photo_path)
+            half_path = photo_path.split('/dataset/')[1]
+            photo_new_name = half_path.split('/')[0] + '_' + half_path.split('/')[1]
+            print(photo_new_name)
+            newpath = os.path.join(new_path, photo_new_name)
+            print(newpath)
+            shutil.copyfile(photo_path, newpath)
+            relative_path = newpath.split('WORK')[1]
+            class_name = half_path.split('/')[0]
+            with open(annotation_name, mode="a", encoding='utf-8') as write_file:
+                file_writer = csv.writer(write_file, delimiter = ",", lineterminator="\r")
+                file_writer.writerow((newpath, relative_path, class_name))
+        except:
+            break
+        """
+        Копирование фото во новый первый датасет и запись во 2ю аннотацию
+        """
+
+
+
+
+def copying_dataset_2(Iter, annotation_name, new_path, numbers):
+    while(True):
+        try: 
+            print(new_path)         
+            photo_path = next(Iter)
+            print(photo_path)
+            photo_new_name = os.path.join(new_path, str(numbers.pop(0)).zfill(5)) + '.jpg'
+            newpath = os.path.join(new_path, photo_new_name)
+            print(newpath)
+            shutil.copyfile(photo_path, newpath)
+            relative_path = newpath.split('WORK')[1]
+            class_name = photo_path.split('dataset/')[1].split(' bear')[0] + ' bear'
+            with open(annotation_name, mode="a", encoding='utf-8') as write_file:
+                file_writer = csv.writer(write_file, delimiter = ",", lineterminator="\r")
+                file_writer.writerow((newpath, relative_path, class_name))
+        except:
+            break
+    """
+    Функция: запись во 2ю аннотацию и копирование в новый вторый датасет
+    """
+
+
+
+
+
+
+if __name__=="__main__":
+    path = '/home/cossieman2000/WORK/python/'
+    project_name = 'dataset'
+    folders = ['polar bear', 'brown bear']
+
+
+    annotation_name = 'annotation_1.csv'
+    filepath = os.path.join(path, annotation_name)
+    with open(annotation_name, mode="w", encoding='utf-8') as write_file:
+        file_writer = csv.writer(write_file, delimiter = ",", lineterminator="\r")
+        file_writer.writerow(('Абсолютный путь', 'Относительный путь', 'Имя классa'))
+    """
+    создание 1й аннотации
+    """
+
+
+
+    iter1 = iterator_1('polar bear')
+    write_annotation(iter1, annotation_name)
+    """
+    Запись в 1ю аннотацию белых медведей
+    """
+    iter2 = iterator_1('brown bear')
+    write_annotation(iter2, annotation_name)
+    """
+    Запись в 1ю аннотацию бурых медведей
+    """
+
+
+
+
+    project_name = 'new_data_1'
+    folder = 'dataset'
+    fullpath = os.path.join(path, project_name)
+    lab1.create_folder(fullpath)
+    new_path = os.path.join(fullpath, folder)
+    lab1.create_folder(new_path)
+    print(new_path)
+
+    """
+    создал новый первый датасет
+    """
+    annotation_name = 'annotation_2.csv'
+    filepath = os.path.join(new_path, annotation_name)
+    with open(annotation_name, mode="w", encoding='utf-8') as write_file:
+        file_writer = csv.writer(write_file, delimiter = ",", lineterminator="\r")
+        file_writer.writerow(('Абсолютный путь', 'Относительный путь', 'Имя классa'))
+    """
+    создал 2ю аннотацию
+    """
+
+
+
+
+    Iter1 = iterator_1('polar bear')
+    copying_dataset_1(Iter1, annotation_name, new_path)
+    """
+    Запись в 2ю аннотацию и копирование в 1й новый датасет белых медведей
+    """
+    Iter2 = iterator_1('brown bear')
+    copying_dataset_1(Iter2, annotation_name, new_path)
+    """
+    Запись в 2ю аннотацию  и копирование в 1й новый датасет бурых медведей
+    """
+
+
+
+    project_name = 'new_data_2'
+    folder = 'dataset'
+    fullpath = os.path.join(path, project_name)
+    lab1.create_folder(fullpath)
+    new_path = os.path.join(fullpath, folder)
+    lab1.create_folder(new_path)
+    print(new_path)
+    annotation_name = 'annotation_3.csv'
+    filepath = os.path.join(new_path, annotation_name)
+    with open(annotation_name, mode="w", encoding='utf-8') as write_file:
+        file_writer = csv.writer(write_file, delimiter = ",", lineterminator="\r")
+        file_writer.writerow(('Абсолютный путь', 'Относительный путь', 'Имя классa'))
+    numbers = (list(range(1,10001)))
+    random.shuffle(numbers)
+    """
+    создание нового второго датасета и 3ей аннотации, списка рандомных чисел от 0 до 10000
+    """
+
+
+
+
+    Iter1 = iterator_1('polar bear')
+    copying_dataset_2(Iter1, annotation_name, new_path, numbers)
+    """
+    запись белых медведей в 3ю аннотацию и копирование в 2й новый датасет
+    """
+    Iter2 = iterator_1('brown bear')
+    copying_dataset_2(Iter2, annotation_name, new_path, numbers)
+    """
+    запись бурых медведей в 3ю аннотацию и копирование в 2й новый датасет
+    """
